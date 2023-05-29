41d7563202f1f87efc8a86639156d8f23ba8b8d0
Nelsen Septa <nelsenseptahenidar@gmail.com>
2023-05-29 23:25:29 +0700

    deploy to vercel 13

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
 dotenv.config();
 const db = new Sequelize(
   process.env.DBNAME,
   process.env.DBUSERNAME,
   process.env.DBPASS,
   {
+    dialectModule: mysql2,
     host: process.env.DBSERVER,
     dialect: "mysql",
   }
 );
 
-console.log(process.env.DBNAME);
-console.log(process.env.DBPASS);
-console.log(process.env.DBNAME);
-console.log(process.env.DBNAME);
 export default db;
